ebfd4aa4a8911ddf801912e08dafe13c877cb955
alfianfl <alvian.wadada@gmail.com>
2022-04-12 21:32:37 +0700

    add ui login

--- src/components/Commons/Navbar/index.jsx
+++ src/components/Commons/Navbar/index.jsx
@@ -1,9 +1,116 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import './style.scss';
 
-function index() {
+const navList = [
+  {
+    name: 'Beranda',
+    path: '/'
+  },
+  {
+    name: 'Peta UMKM',
+    path: '/peta'
+  },
+  {
+    name: 'Toko',
+    path: '/toko'
+  },
+  {
+    name: 'Produk',
+    path: '/produk'
+  },
+  {
+    name: 'Diskusi',
+    path: '/diskusi'
+  }
+];
+
+function Navbar() {
+  const { pathname } = useLocation();
+  const [path, setPath] = useState('');
+
+  useEffect(() => {
+    setPath(pathname);
+  }, [path]);
+
+  const setActiveNavStyle = (path) => {
+    if (path === pathname) {
+      return 'active';
+    }
+  };
+
   return (
-    <div>index</div>
-  )
+    <nav className="navbar-main bg-white border-gray-200 px-2 sm:px-4 py-6 rounded dark:bg-gray-800">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <img
+          src="/docs/images/logo.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="UMKM Logo"
+        />
+
+        <button
+          data-collapse-toggle="mobile-menu"
+          type="button"
+          className="inline-flex items-center p-2 ml-3  text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="mobile-menu"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+          <svg
+            className="hidden w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
+          <ul className="flex items-center flex-col mt-4 md:flex-row md:mt-0 md:font-medium">
+            {navList.map((nav, index) => (
+              <li key={index}>
+                <NavLink
+                  className={`${() =>
+                    setActiveNavStyle(
+                      nav.path
+                    )} navLink block py-2 pr-4 pl-3 rounded  md:p-0`}
+                  to={nav.path}
+                >
+                  {nav.name}
+                </NavLink>
+              </li>
+            ))}
+            <li>
+              <NavLink to={'/login'}>
+                <button
+                  type="button"
+                  className="button-auth rounded-lg text-sm px-5 py-2.5 mr-2  "
+                >
+                  Masuk / Daftar
+                </button>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default index
+export default Navbar;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from 'react';
+import './style.scss';
 
 function Login() {
   return (
-    <div>Login Page</div>
-  )
+    <div className="form-login max-w-md mt-20 mx-auto bg-white rounded my-8">
+      <div className="text-center flex justicy-center flex-col items-center text-gray-600 py-4">
+        <div className="thumb-image mt-2">
+          <img src="" />
+        </div>
+        <h1 className="font-bold mb-2 mt-4">Selamat Datang</h1>
+        <p className="form-title">Masukkan data Anda untuk mengakses akun</p>
+      </div>
+
+      <div className=" pt-6 pb-16">
+        <div className="form-body px-8 mx-auto">
+          <div className="flex flex-col ">
+            <label className="text-left" htmlFor="email">
+              Email
+            </label>
+            <div className="form-input flex items-center bg-white rounded shadow-md mb-4">
+              <span className="px-3">
+                <svg
+                  className="fill-current text-orange-500  w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M18 2a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2h16zm-4.37 9.1L20 16v-2l-5.12-3.9L20 6V4l-10 8L0 4v2l5.12 4.1L0 14v2l6.37-4.9L10 14l3.63-2.9z" />
+                </svg>
+              </span>
+              <input
+                className="w-full h-12 focus:outline-none"
+                type="email"
+                name="email"
+                placeholder="Masukan email Anda"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col ">
+            <label className="text-left" htmlFor="password">
+              Password
+            </label>
+            <div className="form-input flex items-center bg-white rounded shadow-md mb-4">
+              <span className="px-3">
+                <svg
+                  className="fill-current text-orange-500 w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M4 8V6a6 6 0 1 1 12 0h-3v2h4a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                </svg>
+              </span>
+              <input
+                className="w-full h-12 focus:outline-none"
+                type="password"
+                name="password"
+                placeholder="Masukan password Anda"
+              />
+            </div>
+          </div>
+          <div className="button-login w-full my-4">
+            <button className="bg-orange-500 w-full  text-white rounded shadow-md px-6 py-2">
+              Masuk
+            </button>
+          </div>
+          <div className="text-sm">
+            Belum punya akun ?{' '}
+            <span className="regist-link">Daftar atau Registrasi</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
